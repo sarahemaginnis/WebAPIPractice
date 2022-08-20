d00295de95ae33ce3af91eb6e66c8cb7130fa495
sarahemaginnis <sarahemaginnis@gmail.com>
2022-08-20 09:54:47 -0500

    Add project files.

---
+++ WebAPIPractice/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPIPractice.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }  
+        public string ZipCode { get; set; }
+        public string ProfilePicURL { get; set; }
+    }
+}
